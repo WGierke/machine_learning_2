73be2c13a53c79b14452c40d90582acd3d6badcb
berleon <github@leon-sixt.de>
2017-05-31 23:14:06 +0200

    Add 1b and fixed 1a

--- exercise05/kernel.py
+++ exercise05/kernel.py
@@ -5,9 +5,13 @@ from tqdm import tqdm
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--length', help='Length of time series', required=True)
-    parser.add_argument('-n', '--amount', help='Amount of time series', required=True)
-    parser.add_argument('-i', '--iterations', help='Iterations that should be performed', required=True)
+    parser.add_argument('-d', '--length',
+                        help='Length of time series', required=True)
+    parser.add_argument('-n', '--amount',
+                        help='Amount of time series', required=True)
+    parser.add_argument('-i', '--iterations',
+                        help='Iterations that should be performed',
+                        required=True)
     args = parser.parse_args()
 
     n, d = int(args.length), int(args.amount)
